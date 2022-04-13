4ac4e35d8e325132ac189d416c94992082861c8f
Arnaud Avocat Gros <a.avocatg@gmail.com>
2022-04-13 16:45:16 +0300

    add icon, images and paragraph

---
+++ sw.js
@@ -0,0 +1,43 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  './images/hello-icon-128.png',
+  './images/hello-icon-144.png',
+  './images/hello-icon-152.png',
+  './images/hello-icon-192.png',
+  './images/hello-icon-256.png',
+  './images/hello-icon-512.png',
+  './images/pwa.png'
+  //add other files
+];
+
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', (event) => {
+    event.waitUntil((async () => {
+          try {
+            const cache = await caches.open('v1');
+            return cache.addAll(filesToCache);
+          } catch (e) {
+            console.log(e.message);
+          }
+      })());
+  });
+  
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', (event) => {
+    console.log('ServiceWorker Fetch', event.request.url);
+    event.respondWith((async () => {
+          try {
+              const response = await caches.match(event.request);
+              return response || fetch(event.request);
+          } catch (e) {
+              console.log(e.message);
+          }
+      })());
+  });
+  
